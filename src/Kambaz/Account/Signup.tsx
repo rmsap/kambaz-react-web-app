2d541439c2af25b9bf3de76df15f0e13b032fd44
Ryan Saperstein <saperstein.r@northeastern.edu>
2025-01-18 15:59:55 -0500

    Fix typos

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,17 +1,16 @@
-import React from "react";
 import { Link } from "react-router-dom";
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" class="wd-username" />
+      <input placeholder="username" className="wd-username" />
       <br />
-      <input placeholder="password" type="password" class="wd-password" />
+      <input placeholder="password" type="password" className="wd-password" />
       <br />
       <input
         placeholder="verify password"
         type="password"
-        class="wd-password-verify"
+        className="wd-password-verify"
       />
       <br />
       <Link to="/Kambaz/Account/Profile"> Sign up </Link>
